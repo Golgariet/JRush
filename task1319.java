b1c5be92d161297e260aeb3e493335dbd4aa60e1
Golgariet <33132289+Golgariet@users.noreply.github.com>
2018-06-25 13:55:13 +0300

    BufferedWriter

---
+++ task1319.java
@@ -0,0 +1,41 @@
+import java.io.*;
+
+/* 
+Писатель в файл с консоли
+1. Прочесть с консоли имя файла.
+2. Считывать строки с консоли, пока пользователь не введет строку "exit".
+3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой строки.
+
+Требования:
+
+1. Программа должна считывать c консоли имя файла.
+2. Создай и используй объект типа BufferedWriter.
+3. Программа не должна ничего читать из файловой системы.
+4. Программа должна считывать строки с консоли, пока пользователь не введет строку "exit".
+5. Программа должна записать абсолютно все введенные строки (включая "exit") в файл, каждую строчку с новой строки.
+6. Метод main должен закрывать объект типа BufferedWriter после использования.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // напишите тут ваш код
+        BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter( new OutputStreamWriter( new FileOutputStream(reader.readLine())));
+
+        String next;
+        do
+        {
+            next = reader.readLine();
+            writer.write(next);
+
+            if (!next.equals("exit"))
+            {
+                writer.newLine();
+            }
+            else break;
+        }
+        while (!next.equals("exit"));
+        reader.close();
+        writer.close();
+    }
+}
